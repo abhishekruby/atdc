9492bbcc1c8bde682180d38d4cf6f25fb48b1cce
abhishekruby <abhishekruby94@gmail.com>
2026-04-14 19:23:40 +0530

    feat: add Vercel deployment domains to allowed dev origins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['*.ngrok-free.app'],
+  allowedDevOrigins: ['*.ngrok-free.app',"https://atdc-frontend.vercel.app","https://atdc.vercel.app"],
 };
 
 export default nextConfig;
